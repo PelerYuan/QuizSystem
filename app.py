800549ad30dddf279391502c49daf7409cfd5738
PelerYuan <pelerlittlepig@gmail.com>
2025-07-03 19:17:43 +0800

    entrance management

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ def entrance_manage(quiz_id):
         for entrance in Entrance.query.filter_by(quiz_id=quiz_id).all():
             tests.append(
                 {'id': entrance.id, 'quiz_id': entrance.quiz_id, 'name': entrance.name, 'description': entrance.description})
-        return render_template('admin/admin.html', tests=tests)
+        return render_template('admin/manage_entrance.html', tests=tests)
     return redirect(url_for('admin_login'))
 
 
